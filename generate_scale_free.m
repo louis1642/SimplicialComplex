8f0188b123e0cb209181ba60bac0b23c98b6237c
Luigi Catello <62564969+louis1642@users.noreply.github.com>
2024-10-30 15:43:29 +0100

    fixed scalefree algo (or did I?)

--- generate_scale_free.m
+++ generate_scale_free.m
@@ -6,8 +6,13 @@ function generate_scale_free(obj, N0, m_tri, s)
         s (1,1) {mustBeMember(s,[0 1])} = 0
     end
 
+    % check if there are enough nodes in the initial clique to generate
+    % m_tri triangles at each step
+    if (N0 < 2*m_tri +1)
+        error("Initial clique is too small for selected m_tri")
+    end
+
     % initial clique of size N0
-    %obj.A2(1:N0, 1:N0) = ones(N0) - eye(N0);
 
     for i = 1:(N0-2)
         for j = (i+1):(N0-1)
@@ -17,7 +22,6 @@ function generate_scale_free(obj, N0, m_tri, s)
         end
     end
 
-    % existingEdges = nchoosek(1:N0, 2);
 
     % for each node not in the starting clique
     for i = (N0+1):obj.N
@@ -26,7 +30,6 @@ function generate_scale_free(obj, N0, m_tri, s)
         selectedEdges = select_edges(obj, m_tri, s);
         for newTri = 1:m_tri
             obj.Add2Edge(i, selectedEdges(newTri, 1), selectedEdges(newTri, 2));
-            % existingEdges = [existingEdges; [i, selectedEdges(newTri, 1), selectedEdges(newTri, 2)]];
         end
     end
 
@@ -56,8 +59,28 @@ function edges = select_edges(obj, m_tri, s)
             t = t + 1;
         end
         
-    else
+    else        % preferential
         % TODO
-        error('Yet to implement')
+        % number of triangles edge ij belongs to
+        TntriaEd = triu(obj.A2 .* (obj.A2^2));
+        % find the indices of edges forming triangles
+        [i, j] = find(TntriaEd > 0);
+        k = TntriaEd > 0;
+        % computing the cumulative probability distribution
+        plink = [i j TntriaEd(k)];
+        pcum = [0; cumsum((plink(:,3)) ./ sum(plink(:,3)))];
+        % apply inverse transform sampling
+        while (true)
+            r = rand(m_tri, 1);
+            d = pcum' - r;
+            temp = diff(sign(d),1,2);
+            [~, idx] = find(temp);
+            % plink(idx,1:2)
+            if (length(unique(plink(idx,1:2)))==2*m_tri)
+                edges(:,:) = plink(idx, 1:2);
+                break;
+            end
+        end
+
     end
 end
